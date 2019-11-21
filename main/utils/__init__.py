6593a4547faff39f00651674902381c5c2bb4e0a
riomat13 <ryo.m1985@yahoo.com>
2019-11-20 17:09:56 -0800

    add an argument

--- main/utils/__init__.py
+++ main/utils/__init__.py
@@ -3,6 +3,15 @@
 
 
 def make_parser():
+    """Build parser for running scripts.
+
+    Given arguments by this are:
+        --no-config: build text tokenizer from scratch
+        -p, --path:  path to model weights if reuse pretrained one
+        --debug:     run with debug mode, this displays detail
+        --no-save:   do not save weights automatically
+    """
+
     import argparse
 
     parser = argparse.ArgumentParser(description='Run model to answer yes/no')
@@ -12,11 +21,16 @@ def make_parser():
     )
     parser.add_argument(
         '-p', '--path', type=str, default=None,
-        help='path to model weights if reuse pretrained'
+        help='path to model weights if reuse pretrained one'
     )
     parser.add_argument(
         '--debug', default=False, action='store_true',
         help='run with debug mode'
+    )
+
+    parser.add_argument(
+        '--no-save', default=True, action='store_false',
+        help='do not save weights automatically'
     )
 
     return parser
