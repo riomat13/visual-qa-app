cee1b6fab7c6b5f0968376665b2d2f10eacf7999
Rio <ryo.m1985@yahoo.com>
2019-11-22 08:07:28 -0800

    update model server to handle error and save log

--- main/models/__main__.py
+++ main/models/__main__.py
@@ -1,26 +1,42 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import logging
 
 if __name__ == '__main__':
     import argparse
     import asyncio
 
-    from main.settings import Config
     from main.models.server import run_server
 
     parser = argparse.ArgumentParser(description='Serve model')
-    parser.add_argument('-H', '--host', type=str,
-                        default=Config.MODEL_SERVER
-                            .get('host', 'localhost'),
+    parser.add_argument('-H', '--host', type=str, default='',
                         help='host machine to make connection')
-    parser.add_argument('-p', '--port', type=int,
-                        default=Config.MODEL_SERVER
-                            .get('port', 12345),
+    parser.add_argument('-p', '--port', type=int, default=0,
                         help='port number to connect')
+    parser.add_argument('-c', '--config', type=str,
+                        default='local',
+                        help='Config type: production, '
+                             'local, development, test')
 
     args = parser.parse_args()
     host = args.host
     port = args.port
+
+    from main.settings import set_config
+    config = args.config
+    # TODO: add production
+    if config not in ('local', 'development', 'test'):
+        log.warning(f'Invalid config: {config} "local" config will be used')
+        config = 'local'
+    set_config(args.config)
+
+    from main.settings import Config
+
+    # if host and port is not set, use from config
+    if not host:
+        host = Config.MODEL_SERVER.get('host', 'localhost')
+    if not port:
+        port = Config.MODEL_SERVER.get('port', 12345)
 
     asyncio.run(run_server(host, port))
--- main/models/server.py
+++ main/models/server.py
@@ -5,45 +5,72 @@ import os.path
 import asyncio
 import logging
 
-from main.utils.loader import load_image_simple as load_image
-from main.orm.models.ml import ModelRequestLog
+from main.utils.preprocess import text_processor
+from main.utils.logger import save_log
+from main.orm.models.ml import PredictionScore, RequestLog
 from main.models.infer import predict_question_type, awake_models
 
 log = logging.getLogger(__name__)
 
+_processor = None
 
-async def save_predict_history(filepath, sentence):
+
+async def save_predict_history(filepath, sentence, pred, log_model):
     # TODO: store results to DB
-    pass
+    filename = os.path.basename(filepath)
+    score = PredictionScore(filename=filename,
+                            question=sentence,
+                            prediction=pred,
+                            log=log_model)
+    score.save()
 
 
+@save_log(RequestLog)
 async def run_prediction(reader, writer):
     global q_type_model
+    global _processor
 
     data = await reader.read(1024)
     filepath, sentence = data.decode().split('\t')
+    log.debug(sentence)
+    sentence = _processor(sentence)
 
-    if not os.path.isfile(filepath):
-        log.warning('Could not find image')
-        pred = predict_question_type(sentence)
+    try:
+        if not os.path.isfile(filepath):
+            log.warning('Could not find image')
+            pred = predict_question_type(sentence)
+        else:
+            # TODO: add prediction pipeline
+            pred = predict_question_type(sentence)
+    except Exception as e:
+        log.error(e)
+        # send error code
+        writer.write(b'<e>')
     else:
-        # process image
-        #img = load_image(filepath)
+        writer.write(pred.encode())
+    finally:
+        await writer.drain()
+        writer.close()
 
-        # TODO: add prediction pipeline
-        pred = predict_question_type(sentence)
+    filename = os.path.basename(filepath)
+    log_model = RequestLog.query().filter_by(filename=filename).first()
+    if log_model is not None:
+        # save the result
+        await save_predict_history(filepath,
+                                   sentence,
+                                   pred,
+                                   log_model)
+    else:
+        log.error('Could not store predict history')
 
-    writer.write(pred.encode())
-    await writer.drain()
-    writer.close()
-
-    # save the result
-    await save_predict_history(filepath, sentence)
 
 
 async def run_server(host, port):
+    global _processor
+
     # load models to make run faster to serve
     awake_models()
+    _processor = text_processor(num_words=15, from_config=True)
 
     server = await asyncio.start_server(
         run_prediction, host, port)
