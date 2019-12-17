ef70849766739f057a5ec4e66ee8f432273a94cc
Rio <ryo.m1985@yahoo.com>
2019-12-16 22:39:44 -0800

    Add general application log model

--- main/orm/models/base.py
+++ main/orm/models/base.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from sqlalchemy import Column, String, Boolean
+from datetime import datetime
+
+from sqlalchemy import Column, String, Boolean, DateTime, Text
 from werkzeug.security import generate_password_hash, check_password_hash
 
 from main.orm.db import Base
@@ -26,3 +28,48 @@ class User(BaseMixin, Base):
 
     def verify_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+
+class AppLog(BaseMixin, Base):
+    __tablename__ = 'app_log'
+
+    logged_time = Column(DateTime, default=datetime.utcnow())
+    # Log Type: e.g. Info, Warning, Error
+    log_type = Column(String(32), nullable=False)
+    # class name if error. e.g. ValueError
+    log_class = Column(String(32), nullable=True)
+    # Log Detail
+    log_text = Column(Text, nullable=False)
+
+    @classmethod
+    def fetch_logs(cls, log_type=None, log_class=None, log_time=None):
+        """Fetch logs by type, class and/or time
+
+        Args:
+            log_type: str
+                log type such as success, warning, error
+            log_class: str
+                class name of log type such as exception class name
+            log_time: str, datetime object, or list of str or datetime
+                start time to fetch logs
+                if it is list, it will be [start_time, end_time]
+        """
+        logs = cls.query()
+
+        if log_type is not None:
+            logs = logs.filter_by(log_type=log_type)
+
+        if log_class is not None:
+            logs = logs.filter_by(log_class=log_class)
+
+        if log_time is not None:
+            if isinstance(log_time, (list, tuple)):
+                logs = logs.filter(
+                    AppLog.logged_time >= log_time[0],
+                    AppLog.logged_time <= log_time[1]
+                )
+            else:
+                logs = logs.filter(AppLog.logged_time>=log_time)
+
+        return logs.all()
+
--- tests/orm/models/test_base.py
+++ tests/orm/models/test_base.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
 
 from .base import _Base
 
-from main.orm.models.base import User
+from main.orm.models.base import User, AppLog
 
 
 class UserModelTest(_Base):
@@ -24,3 +25,38 @@ class UserModelTest(_Base):
 
         user.password = 'new_pwd'
         self.assertTrue(user.verify_password('new_pwd'))
+
+
+class AppLogTest(_Base):
+
+    def test_save_and_fetch_log(self):
+        size = 10
+        target_size = 5
+        for i in range(target_size):
+            log = AppLog(log_type='success',
+                         log_class='TargetException',
+                         log_text=f'this is test log {i}')
+            log.save()
+
+        id_ = log.id
+
+        for i in range(target_size, size):
+            # set earlier time
+            log = AppLog(log_type='fail',
+                         log_class='SomeException',
+                         log_text=f'this is test log {i}',
+                         logged_time=datetime(2000, 1, 1))
+            log.save()
+
+        log = AppLog.get(id_)
+        self.assertEqual(log.log_class, 'TargetException')
+
+        logs = AppLog.fetch_logs(log_type='success')
+        self.assertEqual(len(logs), target_size)
+
+        logs = AppLog.fetch_logs(log_class='TargetException')
+        self.assertEqual(len(logs), target_size)
+
+        logs = AppLog.fetch_logs(log_time=datetime(2010, 1, 1))
+        self.assertEqual(len(logs), target_size)
+
